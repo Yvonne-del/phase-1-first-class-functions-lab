2dbd70d46fa13e8e0e309d4fab0712d6588ee574
Yvonne-del <ys359375@gmail.com>
2025-03-10 11:45:33 +0300

    high order function manipulation

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function (fare){
+        return fare * num
+    }
+}
+
+const fareDoubler  = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+
+function selectDifferentDrivers(drivers, driverSelector){
+    return driverSelector(drivers);
+}
